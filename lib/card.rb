a906bfde932c1f417eb3deabe98133a81b897abd
Patrick Duvall <duvallp208@gmail.com>
2019-03-05 14:10:50 -0500

    implement merge sort

--- lib/card.rb
+++ lib/card.rb
@@ -1,8 +1,8 @@
 
 class Card
-#   RANKS = { "Two" => 2, "Three" => 3, "Four" => 4, "Five" => 5, "Six" => 6, "Seven" => 7, "Eight" => 8, "Nine" => 9, "Ten" => 10, "Jack" => 11, "Queen" => 12, "King" => 13, "Ace" =>14 }
-#
-#   SUITS = {"Clubs" => 1, "Diamonds" => 2, "Hearts" => 3, "Spades" => 4}
+  RANKS = { "2" => 2, "3" => 3, "4" => 4, "5" => 5, "6" => 6, "7" => 7, "8" => 8, "9" => 9, "10" => 10, "Jack" => 11, "Queen" => 12, "King" => 13, "Ace" =>14 }
+
+  SUITS = {"Clubs" => 0.1, "Diamonds" => 0.2, "Hearts" => 0.3, "Spades" => 0.4}
 
 attr_reader :value, :suit
   def initialize(value, suit)
@@ -12,6 +12,10 @@ attr_reader :value, :suit
 
   def exact
     "#{@value} of #{@suit}"
+  end
+
+  def exact_value
+    SUITS[@suit] + RANKS[@value]
   end
 
 
--- lib/deck.rb
+++ lib/deck.rb
@@ -1,33 +1,68 @@
 
+  def quicksort
+      return [] if empty?
+
+      pivot = delete_at(rand(size))
+      left, right = partition(&pivot.method(:>))
+
+      return *left.quicksort, pivot, *right.quicksort
+    end
+
 
 class Deck < Array
 
-  RANKS = { "2" => 2, "3" => 3, "4" => 4, "5" => 5, "6" => 6, "7" => 7, "8" => 8, "9" => 9, "10" => 10, "Jack" => 11, "Queen" => 12, "King" => 13, "Ace" =>14 }
 
-  SUITS = {"Clubs" => 1, "Diamonds" => 2, "Hearts" => 3, "Spades" => 4}
   def cards
     self
   end
 
+#Uses bubble sort
   def my_sort
     swap = true
-      while swap
-        swap = false
-        (self.length - 1).times do |x|
-          if RANKS[self[x].value] > RANKS[self[x+1].value]
-            self[x], self[x+1] = self[x+1],self[x]
-            swap = true
-          elsif RANKS[self[x].value] == RANKS[self[x+1].value]
-            if SUITS[self[x].suit] > SUITS[self[x+1].suit]
-              self[x], self[x+1] = self[x+1],self[x]
-              swap = true
-            end
-          end
+    while swap
+      swap = false
+      (self.length - 1).times do |x|
+        if self[x].exact_value > self[x+1].exact_value
+        self[x], self[x+1] = self[x+1],self[x]
+          swap = true
         end
       end
+    end
     self
   end
 
+  def merge_sort
+    if self.length <= 1
+      self
+    else
+      mid = (self.length / 2).floor
+      left = Deck.new(self[0..mid-1]).merge_sort
+      right = Deck.new(self[mid..self.length]).merge_sort
+      merge(left, right)
+    end
 
+  end
+
+  def merge(left, right)
+    if left.empty?
+      right
+    elsif right.empty?
+      left
+    elsif left[0].exact_value < right[0].exact_value
+      [left[0]] + merge(left[1..left.length], right)
+    else
+      [right[0]] + merge(left, right[1..right.length])
+    end
+  end
+
+  # def quicksort
+  #     return [] if empty?
+  #
+  #     pivot = delete_at(rand(size))
+  #     left, right = partition(&pivot.method(:>))
+  #
+  #     return *left.quicksort, pivot, *right.quicksort
+  #   end
+  # end
 
 end
